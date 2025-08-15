9d9e5c8f99d90a32ba2d490530b0379d2b9a5637
omarshanyour <omarshanyour7@gmail.com>
2025-08-14 18:12:04 -0700

    Create collegeData.js

---
+++ collegeData.js
@@ -0,0 +1,157 @@
+// collegeData.js - Chunk 1
+
+const colleges = [
+  {
+    name: "University of California, Los Angeles (UCLA)",
+    location: "Los Angeles, CA",
+    acceptanceRate: 0.09,
+    avgGPA: 4.18,
+    majors: [
+      {
+        name: "Computer Science",
+        salary: "$110,000",
+        years: 4,
+        admissionLink: "https://www.admission.ucla.edu/Prospect/Adm_fr.htm"
+      },
+      {
+        name: "Psychology",
+        salary: "$57,000",
+        years: 4,
+        admissionLink: "https://www.psych.ucla.edu/undergraduate/prospective-students/"
+      },
+      {
+        name: "Biology",
+        salary: "$60,000",
+        years: 4,
+        admissionLink: "https://www.mcdb.ucla.edu/undergrad/"
+      }
+    ]
+  },
+  {
+    name: "University of California, San Diego (UCSD)",
+    location: "San Diego, CA",
+    acceptanceRate: 0.31,
+    avgGPA: 4.12,
+    majors: [
+      {
+        name: "Biology",
+        salary: "$60,000",
+        years: 4,
+        admissionLink: "https://biology.ucsd.edu/education/undergrad/index.html"
+      },
+      {
+        name: "Engineering",
+        salary: "$95,000",
+        years: 4,
+        admissionLink: "https://jacobsschool.ucsd.edu/"
+      },
+      {
+        name: "Cognitive Science",
+        salary: "$72,000",
+        years: 4,
+        admissionLink: "https://cogsci.ucsd.edu/undergraduates/index.html"
+      }
+    ]
+  },
+export const colleges = [
+  {
+    name: "University of California, Berkeley",
+    link: "https://admissions.berkeley.edu/",
+    location: "Berkeley, CA",
+    tuition: "$15,891 (in-state)",
+    majors: [
+      {
+        name: "Computer Science",
+        acceptanceRate: "5.2%",
+        averageGPA: 4.18,
+        targetThreshold: 4.0
+      },
+      {
+        name: "Psychology",
+        acceptanceRate: "14.7%",
+        averageGPA: 3.85,
+        targetThreshold: 3.75
+      }
+    ]
+  },
+  {
+    name: "University of California, Los Angeles",
+    link: "https://www.admission.ucla.edu/",
+    location: "Los Angeles, CA",
+    tuition: "$13,752 (in-state)",
+    majors: [
+      {
+        name: "Biology",
+        acceptanceRate: "9.6%",
+        averageGPA: 4.15,
+        targetThreshold: 4.0
+      },
+      {
+        name: "Economics",
+        acceptanceRate: "11.3%",
+        averageGPA: 3.9,
+        targetThreshold: 3.8
+      }
+    ]
+  },
+  {
+    name: "University of California, San Diego",
+    link: "https://admissions.ucsd.edu/",
+    location: "La Jolla, CA",
+    tuition: "$14,700 (in-state)",
+    majors: [
+      {
+        name: "Data Science",
+        acceptanceRate: "8.5%",
+        averageGPA: 4.05,
+        targetThreshold: 3.95
+      },
+      {
+        name: "Mechanical Engineering",
+        acceptanceRate: "7.9%",
+        averageGPA: 4.1,
+        targetThreshold: 4.0
+      }
+    ]
+  },
+  {
+    name: "California State University, Long Beach",
+    link: "https://www.csulb.edu/admissions",
+    location: "Long Beach, CA",
+    tuition: "$7,506 (in-state)",
+    majors: [
+      {
+        name: "Nursing",
+        acceptanceRate: "12.1%",
+        averageGPA: 3.7,
+        targetThreshold: 3.6
+      },
+      {
+        name: "Graphic Design",
+        acceptanceRate: "40%",
+        averageGPA: 3.5,
+        targetThreshold: 3.4
+      }
+    ]
+  },
+  {
+    name: "California Polytechnic State University, San Luis Obispo",
+    link: "https://www.calpoly.edu/admissions",
+    location: "San Luis Obispo, CA",
+    tuition: "$10,195 (in-state)",
+    majors: [
+      {
+        name: "Civil Engineering",
+        acceptanceRate: "15.2%",
+        averageGPA: 4.0,
+        targetThreshold: 3.9
+      },
+      {
+        name: "Business Administration",
+        acceptanceRate: "18.5%",
+        averageGPA: 3.85,
+        targetThreshold: 3.75
+      }
+    ]
+  }
+];
